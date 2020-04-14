b41052f94e0b99555ec1c7a2ee7641aace61d77f
Wesen <wesen@WesendeMacBook-Pro.local>
2020-04-14 22:35:34 +0800

    empty framework of vdata

---
+++ vdata/src/main/java/wesen/vdata/VdataApplication.java
@@ -0,0 +1,13 @@
+package wesen.vdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VdataApplication.class, args);
+    }
+
+}
---
+++ vdata/src/test/java/wesen/vdata/VdataApplicationTests.java
@@ -0,0 +1,13 @@
+package wesen.vdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
